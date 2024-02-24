b8ee44e7cb756789138e236e79722067403c47a2
HenchaDev <henchaedits01@gmail.com>
2024-02-24 14:07:34 +0300

    complete profile

--- sshms/apps/users/urls.py
+++ sshms/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import sign_up, user_login, complete_profile, view_profile, logout
+from .views import sign_up, user_login, complete_profile, view_profile, logout, edit_user_info, save_user_info
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('signup/', sign_up, name='sign_up'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('complete-profile/', complete_profile, name='complete_profile'),
     path('profile/', login_required(view_profile, login_url='/users/login/'), name='profile'),
     path('logout/', logout, name='logout'),
+    path('edit_user_info/', edit_user_info, name='edit_user_info'),
+    path('save_user_info/', save_user_info, name='save_user_info'),
 ]
--- sshms/apps/users/views.py
+++ sshms/apps/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 from .forms import SignUpForm, UserLoginForm, ProfileForm
 from .models import UserProfile
 
@@ -28,6 +30,7 @@ def user_login(request):
     else:
         form = UserLoginForm()
     return render(request, 'users/login.html', {'form':form})
+
 @login_required
 def complete_profile(request):
     try:
@@ -43,6 +46,7 @@ def complete_profile(request):
     else:
         form = ProfileForm(instance=profile)
     return render(request, 'users/complete_profile.html', {'form': form})
+
 @login_required
 def view_profile(request):
     try:
@@ -54,3 +58,20 @@ def view_profile(request):
 
 def logout(request):
     return render(request, 'users/logout.html')
+
+def edit_user_info(request):
+    return render(request, 'users/edit_user_info_form.html')
+
+def save_user_info(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        
+        user = request.user
+        user.username = username
+        user.email = email
+        user.save()
+        
+        return JsonResponse({'message': 'User info updated successfully.'}, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
